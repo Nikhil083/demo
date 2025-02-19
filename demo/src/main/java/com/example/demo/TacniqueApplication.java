50b4e622e812c6ce1e1f6ff8ede146aa03bdd42c
Nikhil083 <kanojia.nikhil243@gmail.com>
2025-02-19 18:10:02 +0530

    create service

---
+++ demo/src/main/java/com/example/demo/TacniqueApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacniqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacniqueApplication.class, args);
+	}
+
+}
